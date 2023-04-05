3c0f7ebbf9881556c22b1de834e8ea939ce1ff83
BetinaNowak <betina-svendsen@live.dk>
2023-04-05 07:56:12 +0200

    Styled sidebar menu

--- App Testing/ViewControllers/SidebarViewController.swift
+++ App Testing/ViewControllers/SidebarViewController.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class SidebarViewController: UIViewController {
 
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        containerView.clipsToBounds = false
+        /*containerView.layer.shadowColor = UIColor.black.cgColor
+        containerView.layer.shadowOpacity = 1
+        profileImage.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        containerView.layer.shadowRadius = 30
+        containerView.layer.shadowPath = UIBezierPath(roundedRect: containerView.bounds, cornerRadius: 20).cgPath*/
+        containerView.backgroundColor = UIColor.clear
+        
+        profileImage.backgroundColor = UIColor.clear
+        profileImage.clipsToBounds = true
+        profileImage.layer.cornerRadius = 40
     }
     
     
