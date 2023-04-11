f8aa139aa2542a14dcb2f472a64d12b50f5bd2bc
BetinaNowak <betina-svendsen@live.dk>
2023-04-11 11:15:48 +0200

    Styled profile view controller

--- App Testing/ViewControllers/Menu items/ProfileViewController.swift
+++ App Testing/ViewControllers/Menu items/ProfileViewController.swift
@@ -12,12 +12,46 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var menuBtn: UIBarButtonItem!
     
     
+    @IBOutlet weak var whiteBackground: UIImageView!
+    
+    @IBOutlet weak var personalInfoBtn: UIButton!
+    @IBOutlet weak var eventsBtn: UIButton!
+    @IBOutlet weak var messagesBtn: UIButton!
+    @IBOutlet weak var savedPostsBtn: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
         setMenuBtn(menuBtn)
+        
+        whiteBackground.layer.shadowRadius = 6
+        whiteBackground.layer.shadowOpacity = 1
+        whiteBackground.layer.shadowOffset = CGSize(width: 2, height: 2)
+        whiteBackground.layer.shadowColor = UIColor.gray.cgColor
+        
+        
+        // Style buttons
+        personalInfoBtn.layer.shadowRadius = 4
+        personalInfoBtn.layer.shadowOpacity = 0.5
+        personalInfoBtn.layer.shadowOffset = CGSize(width: 1, height: 1)
+        personalInfoBtn.layer.shadowColor = UIColor.gray.cgColor
+        
+        eventsBtn.layer.shadowRadius = 4
+        eventsBtn.layer.shadowOpacity = 0.5
+        eventsBtn.layer.shadowOffset = CGSize(width: 1, height: 1)
+        eventsBtn.layer.shadowColor = UIColor.gray.cgColor
+        
+        messagesBtn.layer.shadowRadius = 4
+        messagesBtn.layer.shadowOpacity = 0.5
+        messagesBtn.layer.shadowOffset = CGSize(width: 1, height: 1)
+        messagesBtn.layer.shadowColor = UIColor.gray.cgColor
+        
+        savedPostsBtn.layer.shadowRadius = 4
+        savedPostsBtn.layer.shadowOpacity = 0.5
+        savedPostsBtn.layer.shadowOffset = CGSize(width: 1, height: 1)
+        savedPostsBtn.layer.shadowColor = UIColor.gray.cgColor
     }
     
 
