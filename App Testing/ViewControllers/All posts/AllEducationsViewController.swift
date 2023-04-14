1ffeae727ffc7424a64389df35462e087d823ce0
BetinaNowak <betina-svendsen@live.dk>
2023-04-14 12:59:07 +0200

    Search bar added on educations list

--- App Testing/ViewControllers/All posts/AllEducationsViewController.swift
+++ App Testing/ViewControllers/All posts/AllEducationsViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class AllEducationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+class AllEducationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UISearchBarDelegate, UISearchControllerDelegate {
+
     
     @IBOutlet weak var educationsTableView: UITableView!
     
+    
+    @IBOutlet weak var phoneSearchView: UIView!
+    
+    @IBOutlet weak var searchBarContainer: UIView!
+    
+    
     var PostsArray = [Opslag]()
     
+    var searchResultController = UISearchController(searchResultsController: nil)
+    
+    var filteredPosts: [Opslag] = []
     
 
     override func viewDidLoad() {
@@ -29,6 +38,81 @@ class AllEducationsViewController: UIViewController, UITableViewDelegate, UITabl
         
         self.navigationController?.navigationBar.tintColor = UIColor.black
 
+        // Search
+        searchResultController.searchBar.delegate = self
+        view.addSubview(searchBarContainer)
+        self.setupSearchController()
+        
+        
+        // Create TapGestureRecognizer that dismisses keyboard on tap outside searchbar
+        let singleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.singleTap(sender:)))
+        singleTapGestureRecognizer.numberOfTapsRequired = 1
+        singleTapGestureRecognizer.isEnabled = true
+        singleTapGestureRecognizer.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(singleTapGestureRecognizer)
+        
+    }
+    
+    // Function for dismissing keyboard on tap outside searchbar
+    @objc func singleTap(sender: UITapGestureRecognizer) {
+        self.searchResultController.searchBar.resignFirstResponder()
+        
+        self.phoneSearchView.addSubview(self.searchResultController.searchBar)
+        self.educationsTableView.tableHeaderView = self.phoneSearchView
+        self.searchResultController.searchBar.showsCancelButton = false
+        
+    }
+    
+    
+    // Function for setting up the search bar
+    func setupSearchController() {
+
+        self.searchResultController.searchResultsUpdater = self
+        self.searchResultController.delegate = self
+        self.searchResultController.hidesNavigationBarDuringPresentation = false
+        self.searchResultController.navigationItem.hidesSearchBarWhenScrolling = false
+        self.searchResultController.searchBar.placeholder = "Søg.."
+        self.searchResultController.searchBar.delegate = self
+        self.searchResultController.searchBar.searchBarStyle = .minimal
+        self.searchResultController.searchBar.tintColor = UIColor.white
+        
+        self.phoneSearchView.addSubview(self.searchResultController.searchBar)
+        self.educationsTableView.tableHeaderView = self.phoneSearchView
+        
+        //self.searchResultController.navigationItem.searchController = searchResultController
+    
+        //self.searchResultController.searchBar.showsCancelButton = false
+        definesPresentationContext = true
+        
+    }
+
+    
+    
+    // Add searchbar to view
+    override func viewWillLayoutSubviews() {
+
+        super.viewWillLayoutSubviews()
+        self.searchResultController.searchBar.sizeToFit()
+        self.searchResultController.searchBar.frame.size.width = self.phoneSearchView.frame.size.width
+        self.searchResultController.searchBar.frame.size.height = self.phoneSearchView.frame.size.height
+
+    }
+    
+    // When search cancel button is clicked reload tableview
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.endEditing(true)
+            filteredPosts = PostsArray
+        educationsTableView.reloadData()
+        }
+    
+    // Filter out the posts whose titles do not match the typed in search text
+    private func filterPosts(for searchText: String) {
+      filteredPosts = PostsArray.filter { post in
+        return
+          post.titel!.lowercased().contains(searchText.lowercased())
+      }
+        educationsTableView.reloadData()
     }
 
     
@@ -45,11 +129,28 @@ class AllEducationsViewController: UIViewController, UITableViewDelegate, UITabl
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // If searching show filtered posts, else show all
+        if searchResultController.isActive && searchResultController.searchBar.text != "" {
+            return filteredPosts.count
+        }
         return PostsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! AllEducationsTableViewCell
+        
+        
+        let post: Opslag // error occurs even though the value 'post' is used below?
+        
+        // If search is active, show the filtered post in row
+        if searchResultController.isActive && searchResultController.searchBar.text != "" {
+            post = filteredPosts[indexPath.row]
+        } else {
+            post = PostsArray[indexPath.row]
+        }
+        
+        
         
         cell.titelLabel.text = PostsArray[indexPath.row].titel
         cell.beskrivelseLabel.text = PostsArray[indexPath.row].beskrivelse
@@ -129,4 +230,11 @@ extension UIImageView {
 }
     
 
+// Update search results
+extension AllEducationsViewController: UISearchResultsUpdating {
 
+       func updateSearchResults(for searchController: UISearchController) {
+         filterPosts(for: searchController.searchBar.text ?? "")
+       }
+  
+}
