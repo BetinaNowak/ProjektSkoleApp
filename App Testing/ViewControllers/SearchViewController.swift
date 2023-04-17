ca578882e6bf062762e8bcfe4fa2260deddcc006
BetinaNowak <betina-svendsen@live.dk>
2023-04-17 12:40:57 +0200

    Search page created

---
+++ App Testing/ViewControllers/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  App Testing
+//
+//  Created by Betina Svendsen on 17/04/2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
