d388fd932aefc71b1d7d7735f2441e4f5581fa33
BetinaNowak <betina-svendsen@live.dk>
2023-04-28 09:06:41 +0200

    Message regarding application is coming through!

--- App Testing/Models/AnsoegningerModel.swift
+++ App Testing/Models/AnsoegningerModel.swift
@@ -11,10 +11,11 @@ struct Ansoegning: Codable {
     var id: Int?
     var ansoeger_id: Int?
     var ansoeger_navn: String?
+    var ansoeger_email: String?
     var ansoeger_skole: String?
     var ansoeger_klassetrin: String?
     var ansoeger_beskrivelse: String?
-    var praktik_id: String?
+    var praktik_id: Int?
     var praktik_titel: String?
     var praktik_email: String?
     var praktik_virksomhedsnavn: String?
--- App Testing/ViewControllers/Menu Controllers/MessagesViewController.swift
+++ App Testing/ViewControllers/Menu Controllers/MessagesViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-class MessagesViewController: UIViewController {
+class MessagesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
 
     @IBOutlet weak var messagesTableView: UITableView!
     
@@ -16,8 +17,23 @@ class MessagesViewController: UIViewController {
     @IBOutlet weak var whiteBackground: UIImageView!
     
     
+    var ApplicationsArray = [Ansoegning]()
+    
+    var filteredApplicationsArray = [Ansoegning]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        NetworkServiceApplications.sharedObj.getApplications { (Ansoegning) in
+            self.ApplicationsArray = Ansoegning
+            self.messagesTableView.reloadData()
+        }
+        
+        messagesTableView.delegate = self
+        messagesTableView.dataSource = self
+        
 
         setMenuBtn(menuBtn)
         
@@ -27,6 +43,10 @@ class MessagesViewController: UIViewController {
         whiteBackground.layer.shadowOffset = CGSize(width: 2, height: 2)
         whiteBackground.layer.shadowColor = UIColor.gray.cgColor
         
+        self.messagesTableView.rowHeight = 150.0
+        /*messagesTableView.rowHeight = UITableView.automaticDimension
+        messagesTableView.estimatedRowHeight = 200*/
+        
     }
     
 
@@ -35,6 +55,41 @@ class MessagesViewController: UIViewController {
         menuBar.target = revealViewController()
         menuBar.action = #selector(SWRevealViewController.revealToggle(_:))
         view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       // return ApplicationsArray.count
+
+        let count = ApplicationsArray.filter { application in
+            return application.ansoegning_accepteret == 1 && application.ansoeger_id == 1
+        }.count
+            
+        return count
+  
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MessagesTableViewCell
+        
+
+        // Filter ApplicationsArray to get the application object for this row
+            let filteredApplications = ApplicationsArray.filter { application in
+                return application.ansoegning_accepteret == 1 && application.ansoeger_id == 1
+            }
+            
+            // Check if the filteredApplications array contains an application object for this row
+            if indexPath.row < filteredApplications.count {
+                let application = filteredApplications[indexPath.row]
+                cell.beskedLabel?.text = "Din ansøgning fra " + application.praktik_virksomhedsnavn! + " er blevet accepteret!"
+            }
+            
+            return cell
     }
     
 
