e285ec8a30e87c90154ef29672a615d6da834ad8
iganaskret <44360557+iganaskret@users.noreply.github.com>
2023-06-06 14:48:43 +0200

    companies .9

--- App Testing/ViewControllers/All posts/AllCompaniesViewController.swift
+++ App Testing/ViewControllers/All posts/AllCompaniesViewController.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 
-class AllCompaniesViewController: UIViewController,  UITableViewDelegate, UITableViewDataSource {
+class AllCompaniesViewController:  UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchControllerDelegate  {
     
     var allCompanies = [Virksomhed]()
     
